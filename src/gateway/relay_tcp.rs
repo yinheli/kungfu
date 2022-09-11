8bc9001ab3f68f1d09f5f0bf6b671de1090bb59e
yinheli <me@yinheli.com>
2022-09-11 22:30:42 +0800

    chore: revert `RelayTask`

--- src/gateway/relay_tcp.rs
+++ src/gateway/relay_tcp.rs
@@ -1,5 +1,5 @@
 use std::{
-    net::{IpAddr, SocketAddr},
+    net::IpAddr,
     sync::{Arc, Mutex},
 };
 
@@ -8,10 +8,7 @@ use lru::LruCache;
 use prometheus::{register_int_counter_vec, IntCounterVec};
 use rand::seq::SliceRandom;
 use rayon::prelude::{IntoParallelRefIterator, ParallelIterator};
-use tokio::{
-    io::{copy_bidirectional, AsyncWriteExt},
-    net::{TcpListener, TcpStream},
-};
+use tokio::{io::copy_bidirectional, net::TcpListener};
 
 use crate::{
     config::{setting::RuleType, ArcSetting},
@@ -41,124 +38,104 @@ impl Relay {
         let setting = self.setting.clone();
 
         tokio::spawn(async move {
-            while let Ok((stream, remote_addr)) = server.accept().await {
+            while let Ok((mut stream, remote_addr)) = server.accept().await {
                 let nat = nat.clone();
                 let setting = setting.clone();
-                tokio::spawn(Relay::proceed(RelayTask {
-                    setting,
-                    nat,
-                    stream,
-                    remote_addr,
-                }));
-            }
-        });
-    }
+                tokio::spawn(async move {
+                    let session = nat.find(remote_addr.port());
+                    if session.is_none() {
+                        return;
+                    }
 
-    async fn proceed(mut task: RelayTask) {
-        let session = task.nat.find(task.remote_addr.port());
-        if session.is_none() {
-            return;
-        }
+                    let session = session.unwrap();
 
-        let session = session.unwrap();
+                    // proxy (name), target host, target port
+                    let target = find_target(setting.clone(), session);
 
-        // proxy (name), target host, target port
-        let target = find_target(task.setting.clone(), session);
+                    if target.is_none() {
+                        return;
+                    }
 
-        if target.is_none() {
-            return;
-        }
+                    let target = target.unwrap();
 
-        let target = target.unwrap();
+                    let proxy = setting.proxy.iter().find(|&v| v.name == target.0);
 
-        let proxy = task.setting.proxy.iter().find(|&v| v.name == target.0);
+                    if proxy.is_none() {
+                        warn!("proxy ({}) not found", target.0);
+                        return;
+                    }
 
-        if proxy.is_none() {
-            warn!("proxy ({}) not found", target.0);
-            return;
-        }
+                    let proxy = proxy.unwrap();
+                    let proxy_url = random_proxy(&proxy.values);
 
-        let proxy = proxy.unwrap();
-        let proxy_url = random_proxy(&proxy.values);
+                    let outbound = open_proxy(proxy_url, &target.1, target.2).await;
 
-        let outbound = open_proxy(proxy_url, &target.1, target.2).await;
+                    match outbound {
+                        Ok(mut outbound) => {
+                            let _ = stream.set_nodelay(true);
+                            let _ = outbound.set_nodelay(true);
 
-        match outbound {
-            Ok(mut outbound) => {
-                let _ = task.stream.set_nodelay(true);
-                let _ = outbound.set_nodelay(true);
+                            let result = copy_bidirectional(&mut stream, &mut outbound).await;
 
-                let result = copy_bidirectional(&mut task.stream, &mut outbound).await;
+                            if let Ok((up, down)) = result {
+                                if setting.metrics.is_some() {
+                                    lazy_static! {
+                                        static ref RELAY_COUNT: IntCounterVec = register_int_counter_vec!(
+                                            "relay_total",
+                                            "Number of bytes relay by proxy",
+                                            &["action", "proxy"]
+                                        )
+                                        .unwrap();
+                                        static ref RELAY_HOST_COUNT: IntCounterVec = register_int_counter_vec!(
+                                            "relay_host_total",
+                                            "Number of bytes relay by proxy with domain (latest 5k domains)",
+                                            &["action", "proxy", "domain"]
+                                        )
+                                        .unwrap();
+                                        static ref RELAY_COUNT_CACHE: Mutex<LruCache<String, i32>> =
+                                            Mutex::new(LruCache::new(1000));
+                                    }
 
-                if let Ok((up, down)) = result {
-                    if task.setting.metrics.is_some() {
-                        lazy_static! {
-                            static ref RELAY_COUNT: IntCounterVec = register_int_counter_vec!(
-                                "relay_total",
-                                "Number of bytes relay by proxy",
-                                &["action", "proxy"]
-                            )
-                            .unwrap();
-                            static ref RELAY_HOST_COUNT: IntCounterVec = register_int_counter_vec!(
-                                "relay_host_total",
-                                "Number of bytes relay by proxy with domain (latest 5k domains)",
-                                &["action", "proxy", "domain"]
-                            )
-                            .unwrap();
-                            static ref RELAY_COUNT_CACHE: Mutex<LruCache<String, i32>> =
-                                Mutex::new(LruCache::new(1000));
-                        }
+                                    RELAY_COUNT
+                                        .with_label_values(&["upload", &proxy.name])
+                                        .inc_by(up);
+                                    RELAY_COUNT
+                                        .with_label_values(&["download", &proxy.name])
+                                        .inc_by(down);
 
-                        RELAY_COUNT
-                            .with_label_values(&["upload", &proxy.name])
-                            .inc_by(up);
-                        RELAY_COUNT
-                            .with_label_values(&["download", &proxy.name])
-                            .inc_by(down);
+                                    RELAY_HOST_COUNT
+                                        .with_label_values(&["upload", &proxy.name, &target.1])
+                                        .inc_by(up);
+                                    RELAY_HOST_COUNT
+                                        .with_label_values(&["download", &proxy.name, &target.1])
+                                        .inc_by(down);
 
-                        RELAY_HOST_COUNT
-                            .with_label_values(&["upload", &proxy.name, &target.1])
-                            .inc_by(up);
-                        RELAY_HOST_COUNT
-                            .with_label_values(&["download", &proxy.name, &target.1])
-                            .inc_by(down);
-
-                        if let Some((v, _)) =
-                            RELAY_COUNT_CACHE.lock().unwrap().push(target.1.clone(), 1)
-                        {
-                            if !v.eq(&target.1) {
-                                let _ = RELAY_HOST_COUNT.remove_label_values(&[
-                                    "upload",
-                                    &proxy.name,
-                                    &v,
-                                ]);
-                                let _ = RELAY_HOST_COUNT.remove_label_values(&[
-                                    "download",
-                                    &proxy.name,
-                                    &v,
-                                ]);
+                                    if let Some((v, _)) =
+                                        RELAY_COUNT_CACHE.lock().unwrap().push(target.1.clone(), 1)
+                                    {
+                                        if !v.eq(&target.1) {
+                                            let _ = RELAY_HOST_COUNT.remove_label_values(&[
+                                                "upload",
+                                                &proxy.name,
+                                                &v,
+                                            ]);
+                                            let _ = RELAY_HOST_COUNT.remove_label_values(&[
+                                                "download",
+                                                &proxy.name,
+                                                &v,
+                                            ]);
+                                        }
+                                    }
+                                }
                             }
                         }
+                        Err(e) => {
+                            warn!("open proxy: {}", e);
+                        }
                     }
-                }
+                });
             }
-            Err(e) => {
-                warn!("open proxy: {}", e);
-            }
-        }
-    }
-}
-
-struct RelayTask {
-    setting: ArcSetting,
-    nat: Arc<Nat>,
-    stream: TcpStream,
-    remote_addr: SocketAddr,
-}
-
-impl Drop for RelayTask {
-    fn drop(&mut self) {
-        let _ = self.stream.shutdown();
+        });
     }
 }
 
