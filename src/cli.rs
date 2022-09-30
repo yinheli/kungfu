4da3f55f0b0be3529a66a1be93afbda3fdd6a9ad
yinheli <me@yinheli.com>
2022-09-30 20:19:07 +0800

    chore(deps): upgrade clap & others

--- src/cli.rs
+++ src/cli.rs
@@ -1,21 +1,21 @@
 use clap::Parser;
 
 #[derive(Parser, Debug)]
-#[clap(version, about)]
+#[command(author, version, about)]
 pub struct Cli {
     /// Config file
-    #[clap(short, long, value_hint=clap::ValueHint::FilePath, default_value = "config/config.yml")]
+    #[arg(short, long, value_hint=clap::ValueHint::FilePath, default_value = "config/config.yml")]
     pub config: String,
 
     /// Test config
-    #[clap(short, long)]
+    #[arg(short, long)]
     pub test: bool,
 
     /// Disable watch rules update
-    #[clap(long)]
+    #[arg(long)]
     pub disable_watch: bool,
 
     /// Verbose log
-    #[clap(long)]
+    #[arg(long)]
     pub verbose: bool,
 }
