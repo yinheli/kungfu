3a884ae070390019320ca5278dd4ffbc45e9eb08
yinheli <me@yinheli.com>
2025-12-11 11:30:54 +0800

    chore: log without timestamps and remove chrono dependency

--- src/logger.rs
+++ src/logger.rs
@@ -1,5 +1,4 @@
 use crate::cli;
-use chrono::Local;
 use std::io::Write;
 
 pub(crate) fn init(cli: &cli::Cli) {
@@ -13,13 +12,11 @@ pub(crate) fn init(cli: &cli::Cli) {
         .format(|buf, record| {
             writeln!(
                 buf,
-                "{} [{:<5}] {}",
-                Local::now().format("%Y-%m-%dT%H:%M:%S%.3f"),
+                "[{:<5}] {}",
                 record.level(),
                 record.args()
             )
         })
-        // .filter_level(level)
         .filter_module("kungfu", level)
         .init();
 }
